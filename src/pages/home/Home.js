5d144c763157cd3daf535ba439e1a6f4d2116689
shobkeyy <shobkyy@gmail.com>
2023-04-02 23:50:09 +0200

    'lazy-load'

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import Header from '../../components/home/header/Header'
+import React, { Suspense, lazy } from 'react'
 import './home.css'
-import Memberships from '../../components/home/memberships/Memberships'
-import Services from '../../components/home/services/Services'
-import Benefits from '../../components/home/benefits/Benefits'
-import Footer from '../../components/home/footer/Footer'
-import AfterHero from '../../components/home/afterHero/AfterHero'
-import Nav from '../../components/nav/Nav'
 import design from '../../assets/svgs/design + webflow.svg'
+const AfterHero = lazy(() => import('../../components/home/afterHero/AfterHero'))
+const Benefits = lazy(() => import('../../components/home/benefits/Benefits'))
+const Services = lazy(() => import('../../components/home/services/Services'))
+const Memberships = lazy(() => import('../../components/home/memberships/Memberships'))
+
 const Home = () => {
 
     return (
         <div className='home'>
             <section id='home' className='hero'>
-                {/* <Header /> */}
                 <div className='hero_container'>
-                    <h1 className='text_small'><img src={design} className="svg-ico" /> Design & Development Agency</h1>
+                    <h1 className='text_small'><img src={design} alt="" className="svg-ico" /> Design & Development Agency</h1>
                     <h2 className='text_bold'>The unlimited design subscription that does it all.</h2>
                     <p className='text_reg'>One service for all of your creative needs. Quality standards. Painless management. Unlimited requests & revisions.</p>
                     <a href='#plans' className='hero_btn'>I'M ALREADY INTERESTED</a>
@@ -23,10 +20,18 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <AfterHero />
-            <Benefits />
-            <Services />
-            <Memberships />
+            <Suspense>
+                <AfterHero />
+            </Suspense>
+            <Suspense>
+                <Benefits />
+            </Suspense>
+            <Suspense>
+                <Services />
+            </Suspense>
+            <Suspense>
+                <Memberships />
+            </Suspense>
         </div>
     )
 }
